10f1088eb9c3c6461f30d2da127c4d741ea40ecf
Nicolas Lehuen <nicolas@lehuen.com>
2005-03-04 07:10:58 +0000

    License finale.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
- # Copyright (C) 2002 Nicolas Lehuen
+ # Copyright (C) 2004-2005 Nicolas Lehuen <nicolas@lehuen.com>
  #
  # This library is free software; you can redistribute it and/or
  # modify it under the terms of the GNU Lesser General Public
