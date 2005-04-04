9e53676a674073857f43bf021840bb5aed1577a8
Nicolas Lehuen <nicolas@lehuen.com>
2005-04-04 21:04:10 +0000

    Version 0.51

--- setup.py
+++ setup.py
@@ -17,17 +17,36 @@
 import distutils
 from distutils.core import setup, Extension
 
+# patch distutils if it can't cope with the "classifiers" or
+# "download_url" keywords
+if sys.version < '2.2.3':
+    from distutils.dist import DistributionMetadata
+    DistributionMetadata.classifiers = None
+    DistributionMetadata.download_url = None
+
 setup(
     name = "pytst",
-    version = "0.50",
+    version = "0.51",
     author = "Nicolas Lehuen",
     author_email = "nicolas@lehuen.com",
     url = "http://nicolas.lehuen.com/",
+    description = "An implementation of a Ternary Search Tree (TST) in C++ with Python bindings",
     py_modules = ["tst"],
     ext_modules = [
         Extension(
             "_tst",
             ["tst_wrap.cxx"],
         ),
+    ],
+    download_url = "http://nicolas.lehuen.com/download/",
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'Programming Language :: C++',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ]
 )
--- tst.h
+++ tst.h
@@ -39,7 +39,7 @@
 
 #define UNDEFINED_INDEX -1
 
-#define TST_VERSION "0.50"
+#define TST_VERSION "0.51"
 
 template<class S,class T> class tst_node {
 public:
