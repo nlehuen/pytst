f69b688b58915da93e311e5e775901eadb08831d
Nicolas Lehuen <nicolas@lehuen.com>
2005-06-12 09:17:23 +0000

    git-svn-id: https://svn.thecrmcompany.net/svn/development/tst-PythonReference@1321 be9c518f-ea0b-6b40-9c2b-04e185ab2b54

--- python/PythonReference.h
+++ python/PythonReference.h
@@ -1,69 +1,60 @@
-#ifndef __PYTHONREFERENCE__
-#define __PYTHONREFERENCE__
+#ifndef __PYTHONREFERENCE_H__
+#define __PYTHONREFERENCE_H__
+
+#include "Python.h"
+
 class PythonReference {
 public:
-    inline explicit PythonReference();
-    inline explicit PythonReference(PyObject *object, int borrow=1);
-    inline PythonReference(const PythonReference& that);
+    explicit PythonReference() : ref(Py_None) {
+        Py_INCREF(ref);
+    }
+    
+    explicit PythonReference(PyObject *object, int borrow=1) : ref(object) {
+        if(ref==NULL) {
+            throw TSTException("Cannot reference NULL");
+        }
+        if(borrow) {
+            Py_INCREF(ref);
+        }
+    }
+    
+    PythonReference(const PythonReference& that) : ref(that.ref) {
+        Py_INCREF(ref);
+    }
+    
+    PythonReference& operator= (const PythonReference& that) {
+        PyObject* old = ref;
+        
+        if((this->ref = that.ref) != old) {
+            Py_INCREF(ref);
+            Py_DECREF(old);
+        }
+        
+        return *this;
+    }
+    
+    ~PythonReference() {
+        Py_DECREF(ref);
+    }
 
-    inline PythonReference& operator= (const PythonReference& that);
-    inline int operator==(const PythonReference& that);
-    inline int operator!=(const PythonReference& that);
-
-    inline PyObject* get();
-    inline PyObject* lend();
-    inline ~PythonReference();
+    int operator==(const PythonReference& that) {
+        return (ref==that.ref);
+    }
+    
+    int operator!=(const PythonReference& that) {
+        return (ref!=that.ref);
+    }
+    
+    PyObject* get() {
+        return ref;
+    }
+    
+    PyObject* lend() {
+        Py_INCREF(ref);
+        return ref;
+    }
 
 private:
     PyObject* ref;
 };
-
-PythonReference::PythonReference() : ref(Py_None) {
-    Py_INCREF(ref);
-}
-
-PythonReference::PythonReference(PyObject *object, int borrow) : ref(object) {
-    if(ref==NULL) {
-        throw TSTException("Cannot reference NULL");
-    }
-    if(borrow) {
-        Py_INCREF(ref);
-    }
-}
-
-PythonReference::PythonReference(const PythonReference& that) : ref(that.ref) {
-    Py_INCREF(ref);
-}
-
-PythonReference& PythonReference::operator= (const PythonReference& that) {
-    PyObject* old = ref;
-    
-    if((this->ref = that.ref) != old) {
-        Py_INCREF(ref);
-        Py_DECREF(old);
-    }
-    
-    return *this;
-}
-
-int PythonReference::operator==(const PythonReference& that) {
-    return (ref==that.ref);
-}
-
-int PythonReference::operator!=(const PythonReference& that) {
-    return (ref!=that.ref);
-}
-
-PyObject* PythonReference::get() {
-    return ref;
-}
-
-PyObject* PythonReference::lend() {
-    Py_INCREF(ref);
-    return ref;
-}
-
-PythonReference::~PythonReference() {
-    Py_DECREF(ref);
-}
 #endif
