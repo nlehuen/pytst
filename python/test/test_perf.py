113be7546771560d74b24ed22561eff18dabd83e
Nicolas Lehuen <nicolas@lehuen.com>
2005-07-20 10:57:42 +0000

    Test perf.
    git-svn-id: https://svn.thecrmcompany.net/svn/development/tst-PythonReference@1358 be9c518f-ea0b-6b40-9c2b-04e185ab2b54

--- python/test/test_perf.py
+++ python/test/test_perf.py
@@ -1,17 +1,21 @@
 from tst import *
 from time import clock
+from random import shuffle
 
-ITERATIONS = 10000
+print 'TST version : %s'%TST_VERSION
+
+ITERATIONS = range(500000)
+shuffle(ITERATIONS)
 
 t = TST()
 
 c = clock()
-for i in xrange(ITERATIONS):
+for i in ITERATIONS:
     number = '%i'%i
 print 'Build : %f'%(clock()-c)
 
 c = clock()
-for i in xrange(ITERATIONS):
+for i in ITERATIONS:
     number = '%i'%i
     t[number]=i
 print 'Write : %f'%(clock()-c)
@@ -19,18 +23,7 @@ print 'Write : %f'%(clock()-c)
 t.pack()
 
 c = clock()
-for i in xrange(ITERATIONS):
+for i in ITERATIONS:
     number = '%i'%i
-    r = t[number]
-    assert r==i
+    assert t[number]==i
 print 'Read : %f'%(clock()-c)
-
-raw_input("OK")
-
-t.write(file("toto.tst","wb"))
-
-raw_input("OK 2")
-
-t = TST(file("toto.tst","rb"))
-
-raw_input("OK 3")
