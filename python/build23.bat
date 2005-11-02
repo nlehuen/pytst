6e50f751c45c97861914917b71a1fd570e24a8ff
Nicolas Lehuen <nicolas@lehuen.com>
2005-11-02 10:27:53 +0000

    git-svn-id: https://svn.thecrmcompany.net/svn/development/tst-PythonReference@1438 be9c518f-ea0b-6b40-9c2b-04e185ab2b54

--- python/build23.bat
+++ python/build23.bat
@@ -1,5 +1,5 @@
 rmdir /s /q build include
 mkdir include & xcopy ..\include include
 swig -Iinclude -c++ -python tst.i
-E:\python23\python setup.py clean build --compile=mingw32 install sdist bdist_egg bdist_wininst
+C:\python23\python setup.py clean build --compile=mingw32 install sdist bdist_egg bdist_wininst
 rmdir /s /q build include
