88844ad3c27a38862137596951b7705ed29304d1
Nicolas Lehuen <nicolas@lehuen.com>
2006-05-12 14:58:57 +0000

    Mise en place du critère AVL.

--- python/compacttst.py
+++ python/compacttst.py
@@ -34,6 +34,25 @@ class tst_node(object):
             self.right is not None and 1 or 0,
         )
 
+class balance_info(object):
+    __slots__ = ['did_balance','height','balance','right_balance','left_balance']
+    
+    def __init__(self,did_balance=False,height=0,balance=0,right_balance=0,left_balance=0):
+        self.did_balance = did_balance
+        self.height = height
+        self.balance = balance
+        self.right_balance = right_balance
+        self.left_balance = left_balance
+
+    def __repr__(self):
+        return "balance_info(d=%s,h=%s,b=%s,l=%s,r=%s)"%(
+            self.did_balance,
+            self.height,
+            self.balance,
+            self.left_balance,
+            self.right_balance
+        )
+7
 class compact_tst(object):
     """ Implémentation d'un TST compact """
 
@@ -89,7 +108,7 @@ class compact_tst(object):
     
     def __setitem__(self,string,value):
         """ Ecrit dans l'arbre selon la syntaxe tst[string] = value """
-        self.root = self._insert(string,value,0,self.root)
+        self.root, balance = self._insert(string,value,0,self.root)
         assert self[string] == value
 
     def _insert(self,string,value,index,node):
@@ -114,21 +133,40 @@ class compact_tst(object):
             # On a trouvé un point de divergence avant le dernier caractère du
             # noeud, et de la clé, il va donc falloir splitter
             if local_index < len(node.chars) - 1:
-                node = self._split_node(node,local_index)
-            
+                node, balance = self._split_node(node,local_index)
+                assert balance.height == self._compute_balance(node).height, "%s != %s"%(balance, self._compute_balance(node))
+
             # Maintenant que le split est fait, on peut continuer à positionner
             # la nouvelle clé
+            balance = balance_info()
             if diff>0:
-                node.left = self._insert(string,value,index,node.left)
+                node.left, left_balance = self._insert(string,value,index,node.left)
+                assert not (len(node.left.chars)>1 and left_balance.height!=1)
+                balance.did_balance = left_balance.did_balance
+                right_balance = self._compute_balance(node.right)
             else:
-                node.right = self._insert(string,value,index,node.right)
+                node.right, right_balance = self._insert(string,value,index,node.right)
+                assert not (len(node.right.chars)>1 and right_balance.height!=1)
+                balance.did_balance = right_balance.did_balance
+                left_balance = self._compute_balance(node.left)
 
-            # Puisqu'on vient d'effectuer une opération qui a pu perturber
-            # l'équilibre du noeud, on le rétablit. Bien sûr cela peut changer
-            # le noeud devant être retourné à l'appelant.
-            node = self._balance_node(node)
+            balance.height = (len(node.chars)>1 and 1) or max(left_balance.height, right_balance.height) + 1
+            balance.balance = left_balance.height - right_balance.height
+            balance.left_balance = left_balance.balance
+            balance.right_balance = right_balance.balance
 
-            return node
+            assert balance.height == self._compute_balance(node).height, "%s != %s --- %s, %s"%(balance, self._compute_balance(node), left_balance, right_balance)
+
+            if not balance.did_balance:
+                node, balance = self._balance(node,balance)
+
+            if len(node.chars)!=1:
+                balance.height = 1
+                balance.balance = 0
+                balance.left_balance = 0
+                balance.right_balance = 0
+
+            return node, balance
 
         elif local_index == len(node.chars):
             # On est arrivé au bout des caractères du noeud
@@ -140,9 +178,9 @@ class compact_tst(object):
                 node.data = value
             else:
                 # On n'est pas au bout de la clé
-                node.next = self._insert(string,value,index,node.next)
+                node.next, next_balance = self._insert(string,value,index,node.next)
         
-            return node
+            return node, self._compute_balance(node)
 
         else:
             # On est arrivé au bout de la clé, mais pas au bout des caractères
@@ -152,12 +190,113 @@ class compact_tst(object):
             # On est au bout de la clé, mais avant la fin des caractères du
             # noeud ; il faut donc splitter, mais au local_index précédent car
             # on a bêtement avancé les deux à la fois aux lignes 105 - 106 
-            node = self._split_node(node,local_index-1)
+            node, balance = self._split_node(node,local_index-1)
 
             # On stocke ensuite la clé et la valeur
             node.data = value
             
-            return node
+            return node, balance
+
+    def _compute_balance(self,node):
+        
+        if node is not None:
+            if len(node.chars)>1:
+                return balance_info(height=1)
+            else:
+                balance = balance_info()
+    
+                left_balance = self._compute_balance(node.left)
+                right_balance = self._compute_balance(node.right)
+    
+                balance.did_balance = False
+                balance.height = max(left_balance.height, right_balance.height) + 1
+                balance.balance = left_balance.height - right_balance.height
+                balance.left_balance = left_balance.balance
+                balance.right_balance = right_balance.balance
+                
+                return balance
+        else:
+            return balance_info()
+
+    def _balance(self,node,balance):
+        if balance.balance > 1:
+            if balance.left_balance > 0:
+                node, balance = self._ll(node,balance)
+            else:
+                node, balance = self._lr(node,balance)
+            balance.did_balance = True
+        elif balance.balance < -1:
+            if balance.right_balance < 0:
+                node, balance = self._rr(node,balance)
+            else:
+                node, balance = self._rl(node,balance)
+            balance.did_balance = True
+
+        assert -2 < balance.balance < 2, "Noeud non balancé : %s"%balance
+        assert -2 < self._compute_balance(node).balance < 2, "Noeud non balancé : %s"%self._compute_balance(node)
+
+        return node, balance
+    
+    def _ll(self,node,balance):
+        # Un déséquilibre LL n'est possible qu'avec un noeud de gauche
+        # n'ayant qu'un caractère 
+        assert len(node.left.chars) == 1
+
+        # On fait la rotation au niveau des liens
+        left_node = node.left
+        node.left = left_node.right
+        left_node.right = node
+        
+        # Et maintenant on ramène tous les caractères du noeud d'origine sauf son
+        # dernier, et on les insère au début du noeud de gauche.
+        # En gros si dans le noeud d'origine on a abcdefG
+        # et dans le noeud de gauche on a juste B
+        # A la fin on a dans le noeud d'origine juste G
+        # et dans le noeud de gauche abcdefB
+        new_char = node.chars.pop()
+        node.chars.append(left_node.chars.pop())
+        left_node.chars.append(new_char)
+        node.chars, left_node.chars = left_node.chars, node.chars 
+
+        # On ajuste la balance en fonction de l'opération effectuée
+        balance.height -= 1
+        balance.balance = 0
+        balance.left_balance = 0
+
+        # Le noeud de gauche prend la place du noeud d'origine dans l'arbre
+        return left_node, balance
+         
+    def _rr(self,node,balance):
+        assert len(node.right.chars) == 1
+
+        right_node = node.right
+        node.right = right_node.left
+        right_node.left = node
+        
+        new_char = node.chars.pop()
+        node.chars.append(right_node.chars.pop())
+        right_node.chars.append(new_char)
+        node.chars, right_node.chars = right_node.chars, node.chars 
+
+        balance.height -= 1
+        balance.balance = 0
+        balance.right_balance = 0
+
+        return right_node, balance
+    
+    def _lr(self,node,balance):
+        if len(node.left.right.chars)>1:
+            node.left.right, right_balance = self._split_node(node.left.right,0)
+        node.left, left_balance = self._rr(node.left,balance_info())
+        node, balance = self._ll(node,balance)
+        return node, balance
+    
+    def _rl(self,node,balance):
+        if len(node.right.left.chars)>1:
+            node.right.left, left_balance = self._split_node(node.right.left,0)
+        node.right, right_balance = self._ll(node.right,balance_info())
+        node, balance = self._rr(node,balance)
+        return node, balance
 
     def _split_node(self,node,local_index):
         assert local_index < len(node.chars)
@@ -176,7 +315,17 @@ class compact_tst(object):
         # la chaîne après le split
         node.chars = node.chars[local_index + 1:]
         
-        return new_node
+        return new_node, balance_info(height=1)
+
+    def _cat_node(self,node,balance):
+        if node is None:
+            return None
+        elif node.next is not None and node.left is None and node.right is None and node.data is None:
+            node.chars.extend(node.next.chars)
+            node.next.chars = node.chars
+            return node.next, balance_info(height=1)
+        else:
+            return node, balance             
 
     def _new_node(self,string,value,index):
         new_node = tst_node()
@@ -193,12 +342,7 @@ class compact_tst(object):
             # sinon on met la clé et la donnée dans ce noeud
             new_node.data = value
         
-        return new_node
-    
-    def _balance_node(self,node):
-        # ici vérifier le critère AVL et faire une rotation LL, RR, RL ou LR
-        # si nécessaire
-        return node
+        return new_node, balance_info(height=1)
     
     def stats(self,node,acc):
         if node == None : return
@@ -216,24 +360,24 @@ class compact_tst(object):
         self.stats(node.right,acc)
         
 if __name__ == '__main__':
-    urls = compact_tst()
-    try:
-        chars = 0
-        for n, l in enumerate(file('url-list.txt','rb')):
-            if n%1000==0 : print n
-            key = l.rstrip()
-            chars += len(key)
-            urls[key] = 0
-    finally:
-        stats = {}
-        urls.stats(urls.root,stats)
-        print n+1, "lignes", chars, "caracteres"
-        for key in sorted(stats.keys()):
-            print "%16s\t%6i\t%6.2f%%"%(
-                key,
-                stats[key],
-                100.0 * stats[key] / stats['nodes']
-            ) 
+#     urls = compact_tst()
+#     try:
+#         chars = 0
+#         for n, l in enumerate(file('url_1000000.csv','rb')):
+#             if n%1000==0 : print n
+#             key = l.rstrip()
+#             chars += len(key)
+#             urls[key] = 0
+#     finally:
+#         stats = {}
+#         urls.stats(urls.root,stats)
+#         print n+1, "lignes", chars, "caracteres"
+#         for key in sorted(stats.keys()):
+#             print "%16s\t%6i\t%6.2f%%"%(
+#                 key,
+#                 stats[key],
+#                 100.0 * stats[key] / stats['nodes']
+#             ) 
 
     t = compact_tst()
     t['nicolas'] = 'nicolas'
@@ -253,7 +397,11 @@ if __name__ == '__main__':
     random.shuffle(data)
     
     for i, d in enumerate(data):
-        t[str(d)] = i
+        print d
+        t[str(d)] = d
+#         for i2, d2 in enumerate(data[:i]):
+#             assert t[str(d2)] == d2
+        
     
     for i,d in enumerate(data):
-        assert t[str(d)] == i, "%s => %s != %s"%(d,i,t[str(d)]) 
+        assert t[str(d)] == d, "%s => %s != %s"%(d,i,t[str(d)]) 
